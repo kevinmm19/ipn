77cf84fe8656e9e2c988da8855889301e9dbd708
Kevin Martinez <kevin.martinez@groupeconnect.com>
2018-07-09 16:28:37 -0600

    Update on gulp.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,5 +3,5 @@ mongoose.connect('mongodb://localhost/ipn');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error: '));
 db.once('open', function() {
-    console.log('We are connected!');
+    console.log('We are connected!!!');
 });
